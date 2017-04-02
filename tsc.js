40d993fa51009c8c9d460672ecf3ebb08ce9f75d
Andrew Piper <andrew@threax.com>
2017-04-02 08:19:59 -0400

    Added tsc compiler.

---
+++ tsc.js
@@ -0,0 +1,16 @@
+﻿var exec = require('child_process').exec;
+
+module.exports = function(){
+    var child = exec('tsc',
+        function (error, stdout, stderr) {
+            if (stdout) {
+                console.log('tsc: ' + stdout);
+            }
+            if (stderr) {
+                console.log('tsc error: ' + stderr);
+            }
+            if (error !== null) {
+                console.log('tsc exec error: ' + error);
+            }
+        });
+}
