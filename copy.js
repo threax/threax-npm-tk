411a311252f8fe206e89b7d4a512163870540bfe
Andrew Piper <andrew@threax.com>
2017-04-01 20:01:51 -0400

    Initial revision

---
+++ copy.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+var copy = require('copy');
+
+module.exports = function (files, outDir) {
+    copy(
+        files,
+        outDir,
+        function (err, file) {
+            if (err) { throw err };
+        }
+    );
+};
---
+++ less.js
@@ -0,0 +1,22 @@
+﻿var gulp = require("gulp");
+var less = require('gulp-less');
+var uglifycss = require('gulp-uglifycss');
+var gutil = require('gulp-util');
+var plumber = require('gulp-plumber');
+
+function compileLess(settings) {
+    return gulp.src(settings.files, { base: settings.baseName })
+           .pipe(plumber(function (error) {
+               gutil.log(gutil.colors.red(error.message));
+               this.emit('end');
+           }))
+           .pipe(less({
+               paths: settings.importPaths
+           }))
+           .pipe(uglifycss({
+               "maxLineLen": 80,
+               "uglyComments": true
+           }))
+           .pipe(gulp.dest(settings.dest));
+}
+module.exports = compileLess;
