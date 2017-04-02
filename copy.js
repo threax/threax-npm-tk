3410e24d8eaddbdda96a83a30891c02937f30757
Andrew Piper <andrew@threax.com>
2017-04-01 20:24:49 -0400

    Separated file and dir copy.

--- copy.js
+++ copy.js
@@ -1,13 +1,25 @@
 ﻿"use strict";
 var fs = require('fs-extra');
 
-module.exports = function (files, out) {
+module.exports.file = function (fileIn, fileOut) {
+    fs.copy(
+        fileIn,
+        fileOut,
+        err => {
+            if (err){ return console.error(err); }
+            console.log("Copied " + fileIn + " to " + fileOut);
+        }
+    );
+};
+
+module.exports.dir = function (files, out) {
+    fs.ensureDir(out);
     fs.copy(
         files,
         out,
         err => {
-            if (err) return console.error(err)
-            console.log("success!")
+            if (err){ return console.error(err); }
+            console.log("Copied " + files + " to " + out);
         }
     );
 };
