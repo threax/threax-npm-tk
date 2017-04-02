a517aa79488158be05aeee672ab235cf4aa05884
Andrew Piper <andrew@threax.com>
2017-04-02 11:17:39 -0400

    Added promise to concat.

--- concat.js
+++ concat.js
@@ -1,9 +1,18 @@
 var fs = require('fs-extra');
+var externalPromise = require('threax-npm-tk/externalPromise');
 
 function concat(files, outFile) {
+    var ep = new externalPromise();
+
     var output = files.map((f) => {
         return fs.readFileSync(f).toString();
     }).join(';');
 
-    fs.writeFileSync(outFile, output);
+    fs.writeFile(outFile, output, 
+    (err) => {
+        if (err){ return ep.reject(err); }
+        ep.resolve();
+    });
+
+    return ep.Promise;
 }
